095871c3965ab77dabcd5802564016a00c98ae81
Jacky <Jacky@BILAB-209>
2013-03-14 20:58:34 +0800

    add SAOExtractor

--- src/ntu/im/bilab/jacky/master/patent/SAOExtractor.java
+++ src/ntu/im/bilab/jacky/master/patent/SAOExtractor.java
@@ -2,6 +2,7 @@ package ntu.im.bilab.jacky.master.patent;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -15,31 +16,112 @@ import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.GrammaticalStructureFactory;
 import edu.stanford.nlp.trees.PennTreebankLanguagePack;
 import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeGraphNode;
 import edu.stanford.nlp.trees.TreebankLanguagePack;
 import edu.stanford.nlp.trees.TypedDependency;
 
 public class SAOExtractor {
+	private List<String> subjectTd;
+	private List<String> objectTd;
+	private StanfordParser parser;
+	private GrammaticalStructureFactory gsf;
 
-	public static String[] getSubject(String sent) {
-		StanfordParser parser = StanfordParser.getInstance();
+	public SAOExtractor() {
+		parser = StanfordParser.getInstance();
+		gsf = new PennTreebankLanguagePack().grammaticalStructureFactory();
+		subjectTd = Arrays
+				.asList(new String[] { "nsubj", "nsubjpass", "xsubj" });
+		objectTd = Arrays.asList(new String[] { "dobj", "iobject", "pobj",
+				"prep", "prep_in" });
+	}
+
+	public List<SAOTuple> getSAOTupleList(String sent) {
+		List<SAOTuple> tupleList = new ArrayList<SAOTuple>();
 		Tree parse = parser.parse(sent);
-		parse.pennPrint();
+		List<TypedDependency> tdl = gsf.newGrammaticalStructure(parse)
+				.typedDependenciesCCprocessed();
 
-		TreebankLanguagePack tlp = new PennTreebankLanguagePack();
-		GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+		for (TypedDependency td : tdl) {
+			// get subject
+			if (subjectTd.contains(getName(td))) {
+				TreeGraphNode subject = td.dep();
+				TreeGraphNode predicate = td.gov();
+
+				for (TypedDependency td2 : tdl) {
+
+					if (objectTd.contains(getName(td2))) {
+						TreeGraphNode object = td2.dep();
+						TreeGraphNode predicate2 = td2.gov();
+
+						if (predicate.equals(predicate2)) {
+
+							SAOTuple tuple = new SAOTuple("sent",
+									subject.nodeString(),
+									predicate.nodeString(), object.nodeString());
+							tupleList.add(tuple);
+						}
+					}
+				}
+
+			}
+
+		}
+
+		return tupleList;
+	}
+
+	public List<String> getSubjectList(String sent) {
+		List<String> subjectList = new ArrayList<String>();
+		Tree parse = parser.parse(sent);
 		GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
 		List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
-		TypedDependency td = tdl.get(0);
-		
-		System.out.println(td);
-		System.out.println(td.toString());
-		System.out.println(td.toString(true));
-		System.out.println(td.reln().getShortName());
-		return null;
+
+		for (TypedDependency td : tdl) {
+			if (subjectTd.contains(getName(td)))
+				subjectList.add(getDep(td));
+		}
+
+		return subjectList;
+	}
+
+	public List<String> getObjectList(String sent) {
+		List<String> list = new ArrayList<String>();
+		Tree parse = parser.parse(sent);
+		GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+		List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+		System.out.println(tdl.toString());
+		for (TypedDependency td : tdl) {
+			if (objectTd.contains(getName(td)))
+				if (!list.contains(getDep(td)))
+					list.add(getDep(td));
+		}
+
+		return list;
+	}
+
+	private String getDep(TypedDependency td) {
+		return td.dep().nodeString();
+	}
+
+	private String getGov(TypedDependency td) {
+		return td.gov().nodeString();
+	}
+
+	private String getName(TypedDependency td) {
+		return td.reln().getShortName();
 	}
 
 	public static void main(String[] args) {
-		String s = "My dog also likes sausage.";
-		getSubject(s);
+		String s = "Bell, a company which is based in LA, makes and distributes computer products.";
+		s = "A temporary indicated torque is obtained by taking a conventional dead " +
+				"zone area for a first slip control area, and the value proportional to the " +
+				"slip quantity for a maximum value, this temporary indicated torque is corrected " +
+				"by a correction value according to the tight cornering brake quantity to be the " +
+				"indicated torque of the transfer clutch, and occurrence of any tight cornering" +
+				" brake phenomenon is prevented thereby. In a slip control area after passing a dead zone area (a second slip control area), the slip control is smoothly transferred from the first slip control area to the second slip control area by performing the slip control with a value of the indicated torque according to the slip quantity added to the indicated torque in the first slip control area as the indicated torque, abrupt torque change is prevented, and the vehicle behavior is stabilized thereby.";
+		s = "In a slip control area after passing a dead zone area (a second slip control area), the slip control is smoothly transferred from the first slip control area to the second slip control area by performing the slip control with a value of the indicated torque according to the slip quantity added to the indicated torque in the first slip control area as the indicated torque, abrupt torque change is prevented, and the vehicle behavior is stabilized thereby.";
+		SAOExtractor saoe = new SAOExtractor();
+		
+		System.out.println(saoe.getSAOTupleList(s));
 	}
 }
--- src/ntu/im/bilab/jacky/master/test/SAOExtractorTest.java
+++ src/ntu/im/bilab/jacky/master/test/SAOExtractorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import ntu.im.bilab.jacky.master.patent.SAOExtractor;
 import ntu.im.bilab.jacky.master.patent.SAOExtractorByOpenNLP;
@@ -15,22 +16,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-@RunWith(value = Parameterized.class)
 public class SAOExtractorTest {
 	private SAOExtractor extractor;
-	private String sentence;
-	private String subject;
-	private String predicate;
-	private String object;
-
-	public SAOExtractorTest(String sentence, String subject, String predicate,
-	    String object) {
-		super();
-		this.sentence = sentence;
-		this.subject = subject;
-		this.predicate = predicate;
-		this.object = object;
-	}
 
 	@Before
 	public void setUp() {
@@ -42,46 +29,36 @@ public class SAOExtractorTest {
 		extractor = null;
 	}
 
-	@Parameterized.Parameters
-	public static Collection<Object[]> getParameters() {
-		return Arrays
-		    .asList(new Object[][] {
-		        { "My dog also likes sausage.", "dog", "likes", "sausage" },
-		        {
-		            "A rare black squirrel has become a regular visitor to a suburban garden.",
-		            "squirrel", "become", "visitor" },
-
-		        {
-		            "The slip control is smoothly transferred from the first slip control area to the second slip control area by performing the slip control with a value of the indicated torque according to the slip quantity added to the indicated torque in the first slip control area as the indicated torque.",
-		            "control", "transferred", "sausage" }
-
-		    });
-	}
-
 	@Ignore
 	public void testGetSAOTuple() throws InvalidFormatException, IOException {
 		String data = "A temporary indicated torque is obtained by taking a conventional dead zone area for a first slip control area, and the value proportional to the slip quantity for a maximum value, this temporary indicated torque is corrected by a correction value according to the tight cornering brake quantity to be the indicated torque of the transfer clutch, and occurrence of any tight cornering brake phenomenon is prevented thereby. In a slip control area after passing a dead zone area (a second slip control area), the slip control is smoothly transferred from the first slip control area to the second slip control area by performing the slip control with a value of the indicated torque according to the slip quantity added to the indicated torque in the first slip control area as the indicated torque, abrupt torque change is prevented, and the vehicle behavior is stabilized thereby.";
-		System.out.println(extractor.getSAOTupleList(data));
+		//System.out.println(extractor.getSAOTupleList(data));
 	}
 
 	@Test
-	public void testGetSubject() throws InvalidFormatException, IOException {
-		String expect = subject;
-		String actural = extractor.getSubject(sentence);
+	public void testGetSubjectList() throws InvalidFormatException, IOException {
+		String sentence = "My dog and cat also likes eating sausage.";
+		List<String> expect = Arrays.asList(new String [] {"dog","cat"});
+		List<String> actural = extractor.getSubjectList(sentence);
 		assertEquals(expect, actural);
 	}
 
-	@Ignore
+	@Test
 	public void testGetPredicate() throws InvalidFormatException, IOException {
-		String expect = predicate;
-		String actural = extractor.getPredicate(sentence);
-		assertEquals(expect, actural);
+		//String expect = predicate;
+		//String actural = extractor.getPredicate(sentence);
+		//assertEquals(expect, actural);
 	}
 
-	@Ignore
-	public void testGetObject() throws InvalidFormatException, IOException {
-		String expect = object;
-		String actural = extractor.getObject(sentence);
+	@Test
+	public void testGetObjectList() throws InvalidFormatException, IOException {
+		String sentence = "My dog and cat also likes eating sausage and suger.";
+		List<String> expect = Arrays.asList(new String [] {"sausage","suger"});
+		
+		sentence = "I arrived in Taipei.";
+		expect = Arrays.asList(new String [] {"Taipei"});
+		
+		List<String> actural = extractor.getObjectList(sentence);
 		assertEquals(expect, actural);
 	}
 }
