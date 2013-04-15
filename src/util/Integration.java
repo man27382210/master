56368d83aad44a085306467ff7b250f45afddeff
jackypan1989 <jackypan1989@gmail.com>
2013-04-15 15:02:46 +0800

    update integration

--- src/util/Integration.java
+++ src/util/Integration.java
@@ -1,5 +1,7 @@
 package util;
 
+import item.MakeInstrumentationUtil;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -9,14 +11,15 @@ public class Integration {
 
 	public static void main(String[] args) {
 		try {
+			MakeInstrumentationUtil.make();
 			DBManager m = DBManager.getInstance();
 			String driver = "com.mysql.jdbc.Driver";
-			String url = "jdbc:mysql://140.112.107.209:3306/master";
-			String user = "root";
-			String password = "jacky";
+			String url = "jdbc:mysql://140.112.107.1:3306/jacky";
+			String user = "jacky";
+			String password = "qaz";
 			m.setConfig(driver, url, user, password);
 			m.open();
-			long count = m.count("patents");
+			long count = m.count("patentproject2012.patent_1976");
 			System.out.println(count);
 			m.close();
 		} catch (FileNotFoundException e) {
